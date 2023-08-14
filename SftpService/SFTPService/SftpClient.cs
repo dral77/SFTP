800263de00445fe989b15938e84e899b3b4b54ae
Aleksandar Dragojevic <adragojevic@gmail.com>
2023-08-14 14:21:36 +0200

    Add files via upload

---
+++ SftpService/SFTPService/SftpClient.cs
@@ -0,0 +1,40 @@
+﻿using Renci.SshNet;
+
+
+namespace SFTPService
+{
+    public class SftpClient : Renci.SshNet.SftpClient
+    {
+        public SftpClient(ConnectionInfo connectionInfo) : base(connectionInfo)
+        {
+        }
+
+        public SftpClient(string host, int port, string username, string password) : base(host, port, username, password)
+        {
+        }
+
+        public SftpClient(string host, string username, string password) : base(host, username, password)
+        {
+        }
+
+        public SftpClient(string host, int port, string username, params PrivateKeyFile[] keyFiles) : base(host, port, username, keyFiles)
+        {
+        }
+
+        public SftpClient(string host, string username, params PrivateKeyFile[] keyFiles) : base(host, username, keyFiles)
+        {
+        }
+
+        public void UploadFile(string sourcePath, string targetPath)
+        {
+            FileStream fs = File.OpenRead(sourcePath);
+            UploadFile(fs, targetPath);
+        }
+
+        public void DownloadFile(string sourcePath, string targetPath)
+        {
+            FileStream fs = File.Create(targetPath);
+            DownloadFile(sourcePath, fs);
+        }
+    }
+}
---
+++ SftpService/SftpDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using SFTPService;
+
+SftpClient client = new("sftp.sc-cockpit.com", 22, "AUSTRO_DIESEL", "yVvs6u1eifigJSxBij7x");
+client.Connect();
+
+// list files
+Console.WriteLine("List files in the SFTP directory:");
+//var files = sftpService.ListAllFiles("/test/WCA");
+var files = client.ListDirectory("/test/WCA");
+foreach (var file in files)
+{
+    if (file.IsDirectory)
+    {
+        Console.WriteLine($"\t Directory: [{file.FullName}]");
+    }
+    else if (file.IsRegularFile)
+    {
+        Console.WriteLine($"\t File: [{file.FullName}]");
+    }
+}
+Console.WriteLine();
+
+// download a file
+/*Console.WriteLine("Download a file from the SFTP directory:");
+const string pngFile = @"hi.png";
+File.Delete(pngFile);
+sftpService.DownloadFile(@"/pub/example/imap-console-client.png", pngFile);
+if (File.Exists(pngFile))
+{
+    Console.WriteLine($"\t file {pngFile} downloaded");
+}
+Console.WriteLine();
+*/
+
+// upload a file // not working for this demo SFTP server due to readonly permission
+Console.WriteLine("Upload a file to the SFTP directory:");
+var testFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+//sftpService.UploadFile(testFile, @"/test/WCA/test.txt");
+client.UploadFile(testFile, @"/test/WCA/test.txt");
+//sftpService.DeleteFile(@"/test/WCA/test.txt");
+Console.WriteLine();
+
+client.Disconnect();
